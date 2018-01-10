53b824f435ebeb48dbf96a5f15a0431868a9803b
john <john@veewo.com>
2018-01-10 15:04:12 +0800

    add camera follow

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+	public float smoothTime = 0.3f;
+	
+	Vector3 _currentVelocity;
+
+	void LateUpdate()
+	{
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, target.position, ref _currentVelocity, smoothTime);
+		newPos.z = transform.position.z;
+		transform.position = newPos;
+	}
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		InputEvent();
 	}
-	void FixedUpdate()
+	void LateUpdate()
 	{
 		ResetColliderParams();
 		CheckColliderSide();
